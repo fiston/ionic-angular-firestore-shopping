884b6825a6df11deff713d95840db31720e2d5ea
Jorge Vergara <javebratt@gmail.com>
2019-05-21 09:33:16 -0500

    Adds the interfaces

---
+++ src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyATGUheA0v80RkuT3Wy8-u5UTJCqP2EV4g',
+  authDomain: 'master-firestore-shopping.firebaseapp.com',
+  databaseURL: 'https://master-firestore-shopping.firebaseio.com',
+  projectId: 'master-firestore-shopping',
+  storageBucket: 'master-firestore-shopping.appspot.com',
+  messagingSenderId: '1045676570202',
+  appId: '1:1045676570202:web:10fc56aa094490cc'
+};
